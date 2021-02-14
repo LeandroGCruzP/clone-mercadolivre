004656e94efe77c6ad01f99c42971f6c0b062d16
Leandro Cruz <leandro.programmer@hotmail.com>
2021-02-13 23:00:45 -0300

    add Product on Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Product from "../Product";
+
 import { Container, Wrapper } from "./styles";
 
 const Layout: React.FC = () => {
---
+++ src/components/Product/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+
+import { Container } from "./styles";
+
+const Product: React.FC = () => {
+  return (
+    <Container>
+      <h1>Product</h1>
+    </Container>
+  );
+};
+
+export default Product;
