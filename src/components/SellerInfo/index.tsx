51fca9efb713eea52af422516db1964b11b57ede
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-02-16 20:14:29 -0300

    build structure of SellerInfo

--- src/components/SellerInfo/index.tsx
+++ src/components/SellerInfo/index.tsx
@@ -1,13 +1,64 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  LocationCard,
+  LocationIcon,
+  ReputationCard,
+  ReputationThernometer,
+  ReputationRow,
+  SupportIcon,
+  ClockIcon,
+  More,
+} from './styles';
 
 const SellerInfo: React.FC = () => {
   return (
     <Container>
-      <h1>Seller Info</h1>
+      <Title>Informaçôes sobre o vendedor</Title>
+
+      <LocationCard>
+        <LocationIcon />
+
+        <div>
+          <p>Localização</p>
+          <strong>Sâo Paulo, Sâo Paulo</strong>
+        </div>
+      </LocationCard>
+
+      <ReputationCard>
+        <ReputationThernometer>
+          <li />
+          <li />
+          <li />
+          <li />
+          <li className="active" />
+        </ReputationThernometer>
+
+        <ReputationRow>
+          <div>
+            <strong>1030</strong>
+            <span>vendas nos últimos 2 meses</span>
+          </div>
+          <div>
+            <strong>
+              <SupportIcon />
+            </strong>
+            <span>Presta um bom atendimento</span>
+          </div>
+          <div>
+            <strong>
+              <ClockIcon />
+            </strong>
+            <span>vendas nos últimos 2 meses</span>
+          </div>
+        </ReputationRow>
+      </ReputationCard>
+
+      <More href="#">Ver mais dados do vendedor</More>
     </Container>
   );
-}
+};
 
 export default SellerInfo;
--- src/components/SellerInfo/styles.ts
+++ src/components/SellerInfo/styles.ts
@@ -1,3 +1,23 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Title = styled.div``;
+
+export const LocationCard = styled.div``;
+
+const iconCSS = css``;
+
+export const LocationIcon = styled.div``;
+
+export const ReputationCard = styled.div``;
+
+export const ReputationThernometer = styled.div``;
+
+export const ReputationRow = styled.div``;
+
+export const SupportIcon = styled.div``;
+
+export const ClockIcon = styled.div``;
+
+export const More = styled.div``;
