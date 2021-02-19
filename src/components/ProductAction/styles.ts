dfc4e3befe2c7dec7ca6effe1697674f3190209d
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-02-19 19:22:14 -0300

    type props on Product and include icons

--- src/components/ProductAction/styles.ts
+++ src/components/ProductAction/styles.ts
@@ -1,12 +1,25 @@
 import styled from 'styled-components';
 
+import {
+  HiOutlineHeart,
+  HiOutlineCheck,
+  HiOutlineShieldCheck,
+} from 'react-icons/hi';
+
 export const Container = styled.div``;
 
 export const Condition = styled.div``;
 
 export const Row = styled.div``;
 
-export const HeartIcon = styled.div``;
+export const HeartIcon = styled(HiOutlineHeart)`
+  width: 28px;
+  height: 28px;
+  color: var(--color-blue);
+
+  margin-left: 16px;
+  cursor: pointer;
+`;
 
 export const DispatchTag = styled.div``;
 
@@ -20,13 +33,23 @@ export const StockStatus = styled.div``;
 
 export const MethodCard = styled.div``;
 
-export const CheckIcon = styled.div``;
+export const CheckIcon = styled(HiOutlineCheck)`
+  width: 24px;
+  height: 24px;
+  color: var(--color-green);
+`;
 
 export const Actions = styled.div``;
 
-export const Button = styled.div``;
+type ButtonProps = { solid?: boolean }
+
+export const Button = styled.button<ButtonProps>``;
 
 export const Benefits = styled.div``;
 
-export const ShieldIcon = styled.div``;
-
+export const ShieldIcon = styled(HiOutlineShieldCheck)`
+  width: 20px;
+  height: 20px;
+  color: var(--color-gray);
+  flex-shrink: 0;
+`;
