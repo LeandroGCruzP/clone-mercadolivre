b5b8d0e5d8e1d70cbfbbc99e8857bc1c4839906d
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-02-18 21:45:23 -0300

    add ProductAction on Projet

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import tshirtImage from '../../assets/tshirt.png';
 
 import SellerInfo from '../SellerInfo';
+import ProductAction from '../ProductAction';
 
 import {
   Container,
@@ -32,7 +33,7 @@ const Product: React.FC = () => {
         </Column>
 
         <Column>
-          {/* <ProductAction /> */}
+          <ProductAction />
           <SellerInfo />
 
           <WarrantySection />
---
+++ src/components/ProductAction/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Condition = styled.div``;
+
+export const Row = styled.div``;
+
+export const HeartIcon = styled.div``;
+
+export const DispatchTag = styled.div``;
+
+export const PriceCard = styled.div``;
+
+export const PriceRow = styled.div``;
+
+export const InstallmentsInfo = styled.div``;
+
+export const StockStatus = styled.div``;
+
+export const MethodCard = styled.div``;
+
+export const CheckIcon = styled.div``;
+
+export const Actions = styled.div``;
+
+export const Button = styled.div``;
+
+export const Benefits = styled.div``;
+
+export const ShieldIcon = styled.div``;
+
